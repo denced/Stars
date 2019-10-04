7cb141b41162fe52f388157dcb6eeed83aa57403
Denis Cedeno <denis.cedeno@gmail.com>
2019-10-03 21:36:44 -0400

    Initial Commit

---
+++ Stars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stars
+//
+//  Created by denis cedeno on 10/3/19.
+//  Copyright © 2019 DenCedeno Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
